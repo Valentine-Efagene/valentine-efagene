50bcdef88e5b6167dd63b69596fbb25d5f85f078
Valentine-Efagene <efagenevalentine@gmail.com>
2021-12-21 10:02:31 +0100

    update

--- pages/education/education.js
+++ pages/education/education.js
@@ -9,7 +9,7 @@ import WeldingArm from '../../components/WeldingArm/WeldingArm'
 import Box from '../../components/Box/Box'
 import ArduinoUno from '../../components/ArduinoUno/ArduinoUno'
 import RoboticArmDuckish from '../../components/RoboticArmDuckish/RoboticArmDuckish'
-import THREE from 'three'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 
 export default function Education() {
   return (
@@ -30,64 +30,72 @@ export default function Education() {
           <h2>Notable Areas Studied</h2>
         </div>
         <div className={styles.card}>
-          <div className={styles.roboticsCanvasContainer}>
-            <Canvas className={styles.canvas}>
-              <OrbitControls enableZoom={false} />
-              <ambientLight intensity={0.5} />
-              <axisHelper />
-              <directionalLight position={[-2, 5, 2]} intensity={1} />
-              <Suspense fallback={null}>
-                <RoboticArmDuckish
-                  //position={[]}
-                  scale={0.03}
-                  onClick={(event) => {
-                    console.log(event)
-                  }}
-                />
-              </Suspense>
-            </Canvas>
+          <div className={styles.flex}>
+            <div className={styles.roboticsCanvasContainer}>
+              <Canvas className={styles.canvas}>
+                <OrbitControls enableZoom={true} />
+                <ambientLight intensity={0.5} />
+                <axisHelper />
+                <directionalLight position={[-2, 5, 2]} intensity={1} />
+                <Suspense fallback={null}>
+                  <RoboticArmDuckish
+                    //position={[]}
+                    scale={0.03}
+                    onClick={(event) => {
+                      console.log(event)
+                    }}
+                  />
+                </Suspense>
+              </Canvas>
+            </div>
+
+            <div className={styles.cardNote}>
+              <span>Robotics and control systems engineering</span>
+              <br />
+              <p>
+                Veniam nostrud est et reprehenderit esse proident veniam
+                cupidatat ea sint deserunt elit consequat aliquip. Velit sint
+                enim do culpa deserunt consequat incididunt cillum in qui et
+                ullamco velit. Nulla id anim culpa est laborum et sint sit enim
+                aliquip proident sunt cupidatat sint.
+              </p>
+            </div>
           </div>
-          <div className={styles.cardNote}>
-            <span>Robotics and control systems engineering</span>
-            <br />
-            <p>
-              Veniam nostrud est et reprehenderit esse proident veniam cupidatat
-              ea sint deserunt elit consequat aliquip. Velit sint enim do culpa
-              deserunt consequat incididunt cillum in qui et ullamco velit.
-              Nulla id anim culpa est laborum et sint sit enim aliquip proident
-              sunt cupidatat sint.
-            </p>
-          </div>
+          <KeyboardArrowDownIcon className={styles.down} />
         </div>
         <div className={styles.card}>
-          <div className={styles.roboticsCanvasContainer}>
-            <Canvas className={styles.canvas}>
-              <OrbitControls enableZoom={false} />
-              <ambientLight intensity={0.5} />
-              <axisHelper />
-              <directionalLight position={[-2, 5, 2]} intensity={1} />
-              <Suspense fallback={null}>
-                <RoboticArmDuckish
-                  //position={[]}
-                  scale={0.03}
-                  onClick={(event) => {
-                    console.log(event)
-                  }}
-                />
-              </Suspense>
-            </Canvas>
+          <div className={styles.flex}>
+            <div className={styles.roboticsCanvasContainer}>
+              <Canvas className={styles.canvas}>
+                <OrbitControls enableZoom={true} />
+                <ambientLight intensity={0.5} />
+                <axisHelper />
+                <directionalLight position={[-2, 5, 2]} intensity={1} />
+                <Suspense fallback={null}>
+                  <RoboticArmDuckish
+                    //position={[]}
+                    scale={0.03}
+                    onClick={(event) => {
+                      console.log(event)
+                    }}
+                  />
+                </Suspense>
+              </Canvas>
+            </div>
+
+            <div className={styles.cardNote}>
+              <span>Robotics and control systems engineering</span>
+              <br />
+              <p>
+                Veniam nostrud est et reprehenderit esse proident veniam
+                cupidatat ea sint deserunt elit consequat aliquip. Velit sint
+                enim do culpa deserunt consequat incididunt cillum in qui et
+                ullamco velit. Nulla id anim culpa est laborum et sint sit enim
+                aliquip proident sunt cupidatat sint.
+              </p>
+            </div>
           </div>
-          <div className={styles.cardNote}>
-            <span>Robotics and control systems engineering</span>
-            <br />
-            <p>
-              Veniam nostrud est et reprehenderit esse proident veniam cupidatat
-              ea sint deserunt elit consequat aliquip. Velit sint enim do culpa
-              deserunt consequat incididunt cillum in qui et ullamco velit.
-              Nulla id anim culpa est laborum et sint sit enim aliquip proident
-              sunt cupidatat sint.
-            </p>
-          </div>
+          <KeyboardArrowDownIcon className={styles.down} />
         </div>
       </div>
       <Footer />
