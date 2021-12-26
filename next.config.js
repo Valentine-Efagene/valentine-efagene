39a0ae6062857ed0cdd2d4d9cfafaed2425f4b74
Valentine-Efagene <efagenevalentine@gmail.com>
2021-12-26 12:51:37 +0100

    Trying with next-compose-plugins

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const withPlugins = require('next-compose-plugins')
 const withTM = require('next-transpile-modules')(['@cypress/mount-utils']) // pass the modules you would like to see transpiled
 
-module.exports = withTM({})
+//module.exports = withTM({})
+module.exports = withPlugins([withTM], {
+  // ...
+})
