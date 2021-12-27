95644089444da6f981da95f70c58afa0a20539f4
Valentine-Efagene <efagenevalentine@gmail.com>
2021-12-27 12:11:31 +0100

    I think I have what I want on vercel

--- _next.config.js
+++
@@ -1,7 +0,0 @@
-const withPlugins = require('next-compose-plugins')
-const withTM = require('next-transpile-modules')(['@cypress/mount-utils']) // pass the modules you would like to see transpiled
-
-//module.exports = withTM({})
-module.exports = withPlugins([withTM], {
-  // ...
-})
