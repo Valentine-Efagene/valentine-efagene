052d5b2783c63f8f9219e6665f471f846afed37c
Valentine-Efagene <efagenevalentine@gmail.com>
2021-12-27 11:48:15 +0100

    update

--- pages/index.spec.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { mount } from '@cypress/react'
-import Home from './index'
-
-it('is visible', () => {
-  mount(<Home />)
-  cy.get('div').should('not.be.null')
-})
